1aa9945f8da6624dc0dee0878c19b9deb57ca31d
Zack Ream <zbream@outlook.com>
2015-10-25 00:36:59 -0400

    Rename and restructure Encryption.Analysis project.

--- Cpp/Encryption/Analysis/Properties/AssemblyInfo.cs
+++ Cpp/Encryption/Analysis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DES.Analysis")]
+[assembly: AssemblyTitle("Analysis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DES.Analysis")]
+[assembly: AssemblyProduct("Analysis")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
